5b7ce3cdb9a9fc8dbb5dae3f03cb2cc891b8bf8b
jiakai <gujiakai28@gmail.com>
2023-03-05 07:45:49 +0800

    :bug: fix: 修改底部文字

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -5,7 +5,7 @@ export default class Footer extends Component {
   render() {
     return (
         <div className="footer">
-            <p>© 2023 Gu Jiakai</p>
+            <p>© 2023 Made with ❤️ by ChatGPT</p>
         </div>
     )
   }
